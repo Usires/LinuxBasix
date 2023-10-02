e9c2d441c60c440693fb6f6dfede0a531db72d54
Dirk Steiger <dirk.steiger@gmail.com>
2023-10-02 12:31:24 +0200

    Prepared windows for package selection.

--- LinuxBasics.c
+++ LinuxBasics.c
@@ -13,7 +13,8 @@ char *choices[] = {
 			"Install basics with apt (incl. flatpak)",
 			"Install basics with flatpak",
 			"Install additional fonts",
-			"Show apt and flatpak packages",
+			"Show and preselect apt packages",
+			"Show and preselect flatpak packages",
 			"Exit",
 		  };
 
@@ -38,7 +39,7 @@ int main()
 menu_start:	
 
 	WINDOW *menu_win;
-	// WINDOW *shell_win;
+	WINDOW *select_win;
 
 	int choice = 0;
 	int c;
@@ -54,7 +55,6 @@ menu_start:
 	starty = (24 - HEIGHT) / 3;
 		
 	menu_win = newwin(HEIGHT, WIDTH, starty, startx);
-	// shell_win = newwin(15,80,20,0);
 
 	keypad(menu_win, TRUE);
 	attron(COLOR_PAIR(1) | A_REVERSE | A_BOLD);
@@ -62,9 +62,6 @@ menu_start:
 	attroff(COLOR_PAIR(1));
 	refresh();
 	
-	// box(shell_win, 0, 0);
-	// wrefresh(shell_win);
-
 	print_menu(menu_win, highlight);
 	while(1)
 	{	c = wgetch(menu_win);
@@ -96,11 +93,12 @@ menu_start:
 	// wrefresh(shell_win);
 	// getch();
 	// clrtoeol();
-	refresh();
-	endwin();
+	// refresh();
+	// endwin();
 
 	switch (choice) {
 		case 1:
+			endwin();
 			system("clear && printf '1password install...\n'");
 			system("sudo apt update");
 			system("sudo apt install curl");
@@ -113,16 +111,19 @@ menu_start:
 			system("sudo apt update && printf '\n' && sudo apt install 1password");
 			break;
 		case 2:
+			endwin();
 			system("clear && printf 'APT install...\n\n'");
 			system("sudo apt update && printf '\n'");
 			system("sudo apt install htop mc gdu neovim tilix zip unzip fortune-mod build-essential flatpak neofetch cpufetch preload nala gnome-software-plugin-flatpak");
 			system("flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo");
 			break;
 		case 3:
+			endwin();
 			system("clear && printf 'Flatpak install...\n\n'");
 			system("flatpak install ca.desrt.dconf-editor com.github.tchx84.Flatseal com.discordapp.Discord com.ktechpit.colorwall com.mattjakeman.ExtensionManager com.microsoft.Edge com.valvesoftware.Steam com.visualstudio.code net.cozic.joplin_desktop net.lutris.Lutris org.DolphinEmu.dolphin-emu org.duckstation.DuckStation org.libretro.RetroArch org.mozilla.Thunderbird net.sf.VICE net.fsuae.FS-UAE");
 			break;
 		case 4:
+			endwin();
 			system("clear && printf 'Installing additional fonts...\n\n'");
 			system("wget https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip");
 			system("wget https://download.jetbrains.com/fonts/JetBrainsMono-1.0.3.zip");
@@ -131,11 +132,18 @@ menu_start:
 			system("fc-cache -r -v");
 			break;
 		case 5:
-			system("clear && sudo apt list --installed | less");
-			printf("\n");
-			system("flatpak list --app | less");
+			select_win = newwin(25,40,3,35);
+			box(select_win, 0, 0);
+			wrefresh(select_win);
+			break;
+		case 6:
+			select_win = newwin(25,40,3,35); 
+			box(select_win, 0, 0);
+			wrefresh(select_win);
 			break;
 		default:
+			// refresh();
+			endwin();
 			return 0;
 	}
 	
@@ -143,7 +151,6 @@ menu_start:
 	getchar();
 
 	goto menu_start;
-	// return 0;
 }
 
 
